a8858de0f8ae3ff9fe237ea40e854db1abd40b73
Eduardo Salazar <jesus.sgomez@alumnos.udg.mx>
2020-09-29 00:04:37 -0500

    prueba1

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+// Calculacion Numero E
+
+func main(){
+	var ite int 
+	var calcPrev float64 = 1
+	var valorAct float64 = 1
+	fmt.Scan(&ite)
+	for i := 1; i <= ite; i++ {
+		calcPrev = (calcPrev * float64(i))
+		valorAct = valorAct + (1 / calcPrev)
+	}
+
+	fmt.Println(valorAct)
 }
